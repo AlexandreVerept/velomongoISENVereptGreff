bd9a93bdc4d598deb6de73899f19097603e4a9fb
AV <alexandre.verept@isen.yncrea.fr>
2020-10-12 11:36:58 +0200

    _id added

--- api velo/api_velo.py
+++ api velo/api_velo.py
@@ -1,6 +1,7 @@
 import json
 import requests
 import pandas as pd
+import re
 
 class api_connector():
     """
@@ -12,19 +13,19 @@ class api_connector():
         return response_json.get("records",[])
     
     def get_lille(self):
-        url = "https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=vlille-realtime&q=&rows=2000&facet=libelle&facet=nom&facet=commune&facet=etat&facet=type&facet=etatconnexion"
+        url = "https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=vlille-realtime&q=&rows=-1&facet=libelle&facet=nom&facet=commune&facet=etat&facet=type&facet=etatconnexion"
         return self.get_api(url)
     
     def get_rennes(self):
-        url = "https://data.rennesmetropole.fr/api/records/1.0/search/?dataset=etat-des-stations-le-velo-star-en-temps-reel&q=&rows=2000&facet=nom&facet=etat&facet=nombreemplacementsactuels&facet=nombreemplacementsdisponibles&facet=nombrevelosdisponibles"
+        url = "https://data.rennesmetropole.fr/api/records/1.0/search/?dataset=etat-des-stations-le-velo-star-en-temps-reel&q=&rows=-1&facet=nom&facet=etat&facet=nombreemplacementsactuels&facet=nombreemplacementsdisponibles&facet=nombrevelosdisponibles"
         return self.get_api(url)
     
     def get_lyon(self):
-        url = "https://public.opendatasoft.com/api/records/1.0/search/?dataset=station-velov-grand-lyon&q=&rows=2000&facet=name&facet=commune&facet=bonus&facet=status&facet=available&facet=availabl_1&facet=availabili&facet=availabi_1&facet=last_upd_1"
+        url = "https://public.opendatasoft.com/api/records/1.0/search/?dataset=station-velov-grand-lyon&q=&rows=-1&facet=name&facet=commune&facet=bonus&facet=status&facet=available&facet=availabl_1&facet=availabili&facet=availabi_1&facet=last_upd_1"
         return self.get_api(url)
     
     def get_paris(self):
-        url = "https://opendata.paris.fr/api/records/1.0/search/?dataset=velib-disponibilite-en-temps-reel&q=&rows=2000&facet=name&facet=is_installed&facet=is_renting&facet=is_returning&facet=nom_arrondissement_communes"
+        url = "https://opendata.paris.fr/api/records/1.0/search/?dataset=velib-disponibilite-en-temps-reel&q=&rows=-1&facet=name&facet=is_installed&facet=is_renting&facet=is_returning&facet=nom_arrondissement_communes"
         return self.get_api(url)
     
 class data_cleaner_for_collection():
@@ -123,6 +124,14 @@ def send_collection():
     #clean data
     dc = data_cleaner_for_collection()
     response = dc.clean_all(dflille, dfrennes, dflyon, dfparis)
+    
+    #add the _id
+    idlist = []
+    for i, row in response.iterrows():
+        idlist.append(re.sub('[\W\_]', "", str(row['Geo'])))
+    
+    response = pd.concat([response,pd.DataFrame(idlist, columns = ['_id'])], axis=1, join='inner')
+    
     
     return(response.to_dict('records'))
     
