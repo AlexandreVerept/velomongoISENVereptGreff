1e8d8196d711a2d4b193b5f66ecabe801fb4ae64
jean-loup <jeanloup.greff@gmail.com>
2020-10-19 11:53:09 +0200

    find a station
    not a perfect match with only few letters atm

---
+++ api velo/find_with_some_letters.py
@@ -0,0 +1,16 @@
+import pymongo
+import dns # required for connecting with SRV
+import time
+import api_velo
+
+client = pymongo.MongoClient("mongodb+srv://admin:FzM8WTPuY5@cluster0.lgxev.gcp.mongodb.net/test?w=majority")
+db = client.get_database('Locations')
+db.global_velo.create_index([('Name','text')])
+
+def search(station):
+
+    cur = db.global_velo.find({"$text": { "$search": station.upper() }})
+    for doc in cur:
+        print(doc)
+
+
